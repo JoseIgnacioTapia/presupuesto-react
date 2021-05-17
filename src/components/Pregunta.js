7c4d85fd6093e2bcd85f9bf3a9784b2000cf0f25
JoseIgnacioTapia <josignaciot@gmail.com>
2021-05-17 16:35:52 -0300

    Getting the Input Value

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+  // Definir el State
+  const [cantidad, guardarCantidad] = useState(0);
+
+  // Función que lee el presupuesto
+  const definirPresupuesto = e => {
+    console.log( parseInt(e.target.value, 10) );
+  }
+
+  // Submit para definir el presupuesto
+  const agregarPresupuesto = e => {
+    e.preventDefault();
+
+    // Validar
+
+    // Si se pasa la validación
+
+  }
+
   return ( 
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
 
-      <form>
+      <form
+        onSubmit={agregarPresupuesto}
+      >
         <input 
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
         />
 
         <input
